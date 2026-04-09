66645703e540316b9d6f1123885bf5dd8deeb8da
sanc <sanchit.pahadi4u@gmail.com>
2026-04-09 11:59:44 +0000

    its done wowo

---
+++ build/shader.fs
@@ -0,0 +1,40 @@
+#version 330 core
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 viewPos;
+uniform sampler2D texture0;
+
+void main()
+{
+    // Sample texture
+    vec4 texColor = texture(texture0, TexCoord);
+    vec3 texRGB = texColor.rgb;
+
+    // Ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    // Combine: use texture color instead of objectColor
+    vec3 result = (ambient + diffuse + specular) * texRGB;
+    FragColor = vec4(result, texColor.a);
+}
